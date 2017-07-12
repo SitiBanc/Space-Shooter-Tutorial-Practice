fefc1c55fd19c1fcca4c36b94c50ca93070414bf
Hsin-Yi Chen <hychen.iim05g@g2.nctu.edu.tw>
2017-07-12 11:37:03 +0800

    13 - Audio
    Sound Effect Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,20 +19,30 @@ public class PlayerController : MonoBehaviour {
     public Transform shotSpawn;
     public float fireRate;
     private float nextFire = 0.0f;
+    private AudioSource audioSource;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+        /*
+         *audioSource = GetComponent<AudioSource>();
+         */
     }
 
     void Update() {
+        // Shoot the lazer bolt
         if(Input.GetButton("Fire1") && Time.time > nextFire) {
             nextFire = Time.time + fireRate;
             // We don't want our bolt tilt（change rotation） like our ship does and we need to  force the y position to 0.0f（tilt the ship may change the y position of the ship）
             Instantiate(shot, new Vector3(shotSpawn.position.x, 0.0f, shotSpawn.position.z), Quaternion.Euler(0.0f, 0.0f, shotSpawn.rotation.x));
+            // Add sound effect
+            /*
+             *audioSource.Play();
+             */
         }
     }
 
     void FixedUpdate() {
+        // Move the player ship
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
