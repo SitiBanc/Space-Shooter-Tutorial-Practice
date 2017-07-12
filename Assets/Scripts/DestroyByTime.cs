bc0b4e7dc562ec8241809204fb6979a68303ddc5
Hsin-Yi Chen <hychen.iim05g@g2.nctu.edu.tw>
2017-07-12 10:20:33 +0800

    12 - Spawning Waves
    Destroy By Time Script Added（destroy explosion gameObject）

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour {
+    public float lifetime;
+
+    // Wait for 'lifetime' after the gameObject is instantiated than destroy it
+    void Start() {
+        Destroy(gameObject, lifetime);
+    }
+}
