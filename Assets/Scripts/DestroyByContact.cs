816daf906011ab2b813f424b965f416a92f33539
Hsin-Yi Chen <hychen.iim05g@g2.nctu.edu.tw>
2017-07-13 17:30:48 +0800

    Extending Enemy Bolt
    Add WeaponController for enemy ship to fire
    Add Enemy Bolt to prefabs for shooting
    Edit DestroyByContact so that the enemy ship, enemy bolt and asteroid won't collide and destroy each other

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
     // Add explosion effect upon detroying GameObject
-    public GameObject explosion;
+    public GameObject selfExplosion;
     public GameObject playerExplosion;
     // Add score after destroy the asteroid
     public int scoreValue;  // The score of destroying an asteroid
@@ -20,32 +20,39 @@ public class DestroyByContact : MonoBehaviour {
         if (gameControllerObject != null) {
             gameController = gameControllerObject.GetComponent<GameController>();
         }
+
         // Just in case something goes wrong and we can't find the gameController
         if (gameControllerObject == null) {
             Debug.Log("Cannot find 'GameController' script.");
         }
     }
-
-    // When other Collider（bolt） enter（asteroid） destroy both the bolt and the asteroid
-    void OnTriggerEnter(Collider other)
-    {
-        // Ignore the collison with boundary
-        if (other.CompareTag("Boundary")) {
+    
+    void OnTriggerEnter(Collider other) {
+        // Ignore the collison with boundary or enemy（collision between asteroid and enemy  bolt or enemy ship）
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy")) {
             // return to Unity（end the execution this function）
             return;
         }
-        // Add explosion effect
-        Instantiate(explosion, transform.position, transform.rotation);
+
+        /* Add self explosion effect if there's one
+         * When this script is attached to asteroid or enemy ship（not enemy bolt）
+         */
+        if (selfExplosion != null) {
+            Instantiate(selfExplosion, transform.position, transform.rotation);
+        }
+
         // Add explosion effect of player ship and execute the GameController.GameOver() function if （asteroid） collide to player ship
         if (other.CompareTag("Player")) {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
             gameController.GameOver();
         }
+
         // Add score
         gameController.AddScore(scoreValue);
 
         // Destroy the other GameObject（bolt）
         Destroy(other.gameObject);
+
         // Destroy the GameObject this script is attached to（asteroid）
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+    // After the Enemy Ship is spawned, wait for 'delay' then continuously fire() at fireRate
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float delay;
+    public float fireRate;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Fire", delay, fireRate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Fire() {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+    }
+}
